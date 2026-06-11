fb62bb7c2f955364276352de4c847225305c10ba
roshan-1904 <gowthamjoshav@gmail.com>
2026-06-11 15:35:05 +0530

    UPDATED: 2024-06-17 12:34:56

--- server/index.js
+++ server/index.js
@@ -23,8 +23,22 @@ app.use((req, res, next) => {
     next();
 });
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://techfrogeworkshop.netlify.app',
+    process.env.CLIENT_URL
+].filter(Boolean);
+
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true
 }));
 app.use(express.json());
